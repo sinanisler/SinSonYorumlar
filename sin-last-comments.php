5727dbafd1f45c9aab63913d6d8b2e276fc06129
Sinan İŞLER <sinan@sinanisler.com>
2013-02-09 23:50:39 +0200

    Update sin-last-comments.php

--- sin-last-comments.php
+++ sin-last-comments.php
@@ -8,8 +8,6 @@ Author: Sinan İŞLER
 Version: 1.0
 Author URI: http://www.sinanisler.com/
 
-Copyright 2012  Sinan İŞLER
-
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as 
     published by the Free Software Foundation.
